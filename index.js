187feff91cee90bff1c9e51073aea5b5d75f3249
cryptomehedi <classicalmehedi@gmail.com>
2022-07-01 21:40:02 +0600

    mongo user

--- index.js
+++ index.js
@@ -12,14 +12,15 @@ app.use(cors())
 app.use(express.json())
 
 // mongodb connection
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ouoh3.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ouoh3.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://heytodobro:3UKGnY8s7HohA1s8@cluster0.ouoh3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 console.log(uri);
 
 async function run(){
     try {
         await client.connect()
-        const tasksCollection = client.db('TasksList').collection('tasks') 
+        const tasksCollection = client.db('TasksList').collection('tasks')
 
         app.post('/task', async(req, res)=> {
             const task = req.body.task
