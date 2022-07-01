f3fb6ad54214c31ec83b89c00db0be95cfe32b10
cryptomehedi <classicalmehedi@gmail.com>
2022-07-01 21:24:50 +0600

    fixed v1

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb'); 
 const app = express();
 const port = process.env.PORT || 4000
 require('dotenv').config();
