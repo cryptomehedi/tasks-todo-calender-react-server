2d18ce3a65dad81a7081e10492b35c9996368f74
cryptomehedi <classicalmehedi@gmail.com>
2022-07-01 10:59:02 +0600

    post

--- index.js
+++ index.js
@@ -11,7 +11,25 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json())
 
+// mongodb connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ouoh3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try {
+        await client.connect()
+        const tasksCollection = client.db('TasksList').collection('tasks') 
+
+        app.post('/task', async(req, res)=> {
+            const task = req.body.task
+            const results = await tasksCollection.insertOne({task})
+            res.send(results)
+        })
+
+        
+    }
+    finally{}
+}
 
 run().catch(err => console.dir(err))
 
